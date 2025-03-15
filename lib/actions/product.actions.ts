013b54f5948a176adda01c46cab1f32d063b6194
liaHi19 <gn.natalia19@gmail.com>
2025-03-15 17:17:01 +0000

    feat: adding basic ProductDetails Page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -5,6 +5,7 @@ import { prisma } from "@/db/prisma";
 import { LATEST_PRODUCT_LIMIT } from "../constants";
 import { convertToPlainObject } from "../utils";
 
+// Get Latest Products
 export async function getLatestProducts() {
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCT_LIMIT,
@@ -12,4 +13,12 @@ export async function getLatestProducts() {
   });
 
   return convertToPlainObject(data);
+}
+
+// Get Single Product By Slug
+
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
