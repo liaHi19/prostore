92636f017a09eed0171bee38549ceefc8e00e28a
liaHi19 <gn.natalia19@gmail.com>
2025-03-15 15:55:05 +0000

    feat: adding configurations for Prisma

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,23 +1,15 @@
 "use server";
 
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 
 import { LATEST_PRODUCT_LIMIT } from "../constants";
 import { convertToPlainObject } from "../utils";
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCT_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
-  const transformedData = data.map((product) => ({
-    ...product,
-    price: product.price.toString(),
-    rating: product.rating.toString(),
-  }));
-
-  return convertToPlainObject(transformedData);
+  return convertToPlainObject(data);
 }
