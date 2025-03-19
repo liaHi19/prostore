864012fd21edc43c0d775466ae6d23f00da281f3
liaHi19 <gn.natalia19@gmail.com>
2025-03-19 16:16:03 +0000

    feat: adding nextauth

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
