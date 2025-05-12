360012c895b30015f337a470373e7d07e0122fd3
liaHi19 <gn.natalia19@gmail.com>
2025-05-12 07:18:59 +0100

    feat: adding ui for ProfileForm

---
+++ app/user/profile/profile-form.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { useToast } from "@/hooks/use-toast";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { updateProfileSchema } from "@/lib/validators";
+
+const ProfileForm = () => {
+  const { data: session, update } = useSession();
+
+  const form = useForm<z.infer<typeof updateProfileSchema>>({
+    resolver: zodResolver(updateProfileSchema),
+    defaultValues: {
+      name: session?.user?.name ?? "",
+      email: session?.user?.email ?? "",
+    },
+  });
+  const {
+    formState: { isValid, isSubmitting, isDirty },
+  } = form;
+
+  const { toast } = useToast();
+
+  const onSubmit = () => {};
+  return (
+    <Form {...form}>
+      <form
+        className="flex flex-col gap-5"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <div className="flex flex-col gap-5">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    disabled
+                    placeholder="Email"
+                    className="input-field"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    placeholder="Name"
+                    className="input-field"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button
+          type="submit"
+          size="lg"
+          className="button col-span-2 w-full cursor-pointer"
+          disabled={!isValid || !isDirty || isSubmitting}
+        >
+          {isSubmitting ? "Submitting..." : "Update Profile"}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default ProfileForm;
