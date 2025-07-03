f4dc68e042a0a03500ba8ae90e9a4c4434d98e0e
liaHi19 <gn.natalia19@gmail.com>
2025-07-03 12:14:49 +0100

    feat: adding ProductCarousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,17 @@
+import ProductCarousel from "@/components/shared/header/product/product-carousel";
 import ProductList from "@/components/shared/header/product/product-list";
 
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from "@/lib/actions/product.actions";
 
 export default async function Home() {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
   return (
     <>
+      {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
       <ProductList data={latestProducts} title="Newest arrivals" limit={4} />
     </>
   );
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -126,3 +126,14 @@ export async function getAllCategories() {
 
   return data;
 }
+
+// get featured products
+export async function getFeaturedProducts() {
+  const data = await prisma.product.findMany({
+    where: { isFeatured: true },
+    orderBy: { createdAt: "desc" },
+    take: 4,
+  });
+
+  return convertToPlainObject(data);
+}
