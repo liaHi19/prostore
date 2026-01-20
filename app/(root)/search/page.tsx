d7cdbf5e8b0dfe48c2d945ebd025d2265c5c8dc2
liaHi19 <gn.natalia19@gmail.com>
2026-01-20 10:20:57 +0000

    feat: adding getFilterUrl function

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -24,6 +24,30 @@ const SearchPage = async ({
     sort = "newest",
     page = "1",
   } = await searchParams;
+  // construct the filter url
+
+  const getFilterUrl = ({
+    c,
+    p,
+    r,
+    s,
+    pg,
+  }: Partial<{
+    c: string;
+    p: string;
+    r: string;
+    s: string;
+    pg: string;
+  }>) => {
+    const params = { q, category, price, rating, sort, page };
+    if (c) params.category = c;
+    if (p) params.price = p;
+    if (r) params.rating = r;
+    if (s) params.sort = s;
+    if (pg) params.page = pg;
+
+    return `/search?${new URLSearchParams(params).toString()}`;
+  };
 
   const products = await getAllProducts({
     query: q,
