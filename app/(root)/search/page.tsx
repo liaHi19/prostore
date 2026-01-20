e80b70540740313da71cc8d75d60ebb5a8b159fe
liaHi19 <gn.natalia19@gmail.com>
2026-01-20 15:17:43 +0000

    feat: finishing SearchPage

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,6 +1,54 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 import ProductCard from "@/components/shared/header/product/product-card";
 
-import { getAllProducts } from "@/lib/actions/product.actions";
+import {
+  getAllCategories,
+  getAllProducts,
+} from "@/lib/actions/product.actions";
+import { prices, ratings, sortOrders } from "@/lib/constants";
+import { cn } from "@/lib/utils";
+
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: Promise<
+    Partial<{
+      q: string;
+      category: string;
+      price: string;
+      rating: string;
+    }>
+  >;
+}) {
+  const {
+    q = "all",
+    category = "all",
+    price = "all",
+    rating = "all",
+  } = await searchParams;
+
+  const isQuerySet = q && q !== "all" && q.trim() !== "";
+  const isCategorySet =
+    category && category !== "all" && category.trim() !== "";
+  const isPriceSet = price && price !== "all" && price.trim() !== "";
+  const isRatingSet = rating && rating !== "all" && rating.trim() !== "";
+
+  if (isQuerySet || isCategorySet || isPriceSet || isRatingSet) {
+    return {
+      title: `
+      Search ${isQuerySet ? q : ""} 
+      ${isCategorySet ? `: Category ${category}` : ""}
+      ${isPriceSet ? `: Price ${price}` : ""}
+      ${isRatingSet ? `: Rating ${rating}` : ""}`,
+    };
+  } else {
+    return {
+      title: "Search Products",
+    };
+  }
+}
 
 const SearchPage = async ({
   searchParams,
@@ -57,10 +105,116 @@ const SearchPage = async ({
     sort,
     page: Number(page),
   });
+
+  const categories = await getAllCategories();
   return (
     <div className="grid md:grid-cols-5 md:gap-5">
-      <div className="filter-links">{/* Filters */}</div>
+      <div className="filter-links">
+        {/* Category Links */}
+        <h5 className="text-xl mb-2 mt-3">Department</h5>
+        <div>
+          <ul className="space-y-1">
+            <li>
+              <Link
+                className={cn(
+                  (category === "all" || category === "") && "font-bold"
+                )}
+                href={getFilterUrl({ c: "all" })}
+              >
+                Any
+              </Link>
+            </li>
+            {categories.map((cat) => (
+              <li key={cat.category}>
+                <Link
+                  className={cn(category === cat.category && "font-bold")}
+                  href={getFilterUrl({ c: cat.category })}
+                >
+                  {cat.category}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* Price Links */}
+        <h5 className="text-xl mb-2 mt-8">Price</h5>
+        <div>
+          <ul className="space-y-1">
+            <li>
+              <Link
+                className={cn(price === "all" && "font-bold")}
+                href={getFilterUrl({ p: "all" })}
+              >
+                Any
+              </Link>
+            </li>
+            {prices.map((p) => (
+              <li key={p.value}>
+                <Link
+                  className={cn(price === p.value && "font-bold")}
+                  href={getFilterUrl({ p: p.value })}
+                >
+                  {p.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* Rating Links */}
+        <h5 className="text-xl mb-2 mt-8">Customer Ratings</h5>
+        <div>
+          <ul className="space-y-1">
+            <li>
+              <Link
+                className={cn(rating === "all" && "font-bold")}
+                href={getFilterUrl({ r: "all" })}
+              >
+                Any
+              </Link>
+            </li>
+            {ratings.map((r) => (
+              <li key={r}>
+                <Link
+                  className={cn(rating === r.toString() && "font-bold")}
+                  href={getFilterUrl({ r: `${r}` })}
+                >
+                  {`${r} stars & up`}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
       <div className="md:col-span-4 space-y-4">
+        <div className="flex flex-between flex-col md:flex-row my-4">
+          <div className="flex items-center">
+            {q !== "all" && q !== "" && "Query: " + q}
+            {category !== "all" && category !== "" && " Category: " + category}
+            {price !== "all" && " Price: " + price}
+            {rating !== "all" && " Rating: " + rating + " stars & up"}
+            &nbsp;
+            {(q !== "all" && q !== "") ||
+            (category !== "all" && category !== "") ||
+            rating !== "all" ||
+            price !== "all" ? (
+              <Button variant={"link"} asChild>
+                <Link href="/search">Clear</Link>
+              </Button>
+            ) : null}
+          </div>
+          <div>
+            Sort by{" "}
+            {sortOrders.map((s) => (
+              <Link
+                key={s}
+                className={cn("mx-2", sort === s && "font-bold")}
+                href={getFilterUrl({ s })}
+              >
+                {s}
+              </Link>
+            ))}
+          </div>
+        </div>
         <div className="grid grid-cols-1 gap-1 md:grid-cols-3">
           {products.data.length === 0 && <p> No products found</p>}
           {products.data.map((product) => (
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -55,3 +55,30 @@ export const productDefaultValues = {
 export const USER_ROLES = process.env.USER_ROLES
   ? process.env.USER_ROLES.split(", ")
   : ["admin", "user"];
+
+export const prices = [
+  {
+    name: "$1 to $50",
+    value: "1-50",
+  },
+  {
+    name: "$51 to $100",
+    value: "51-100",
+  },
+  {
+    name: "$101 to $200",
+    value: "101-200",
+  },
+  {
+    name: "$201 to $500",
+    value: "201-500",
+  },
+  {
+    name: "$501 to $1000",
+    value: "501-1000",
+  },
+];
+
+export const ratings = [4, 3, 2, 1];
+
+export const sortOrders = ["newest", "lowest", "highest", "rating"];
